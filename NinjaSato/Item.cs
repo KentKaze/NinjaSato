26ecb663b67ff098a9fd3dd891ea37657d8f5f85
KentKaze <kentnewbie@hotmail.com>
2021-05-19 22:21:16 +0900

    Add project files.

---
+++ NinjaSato/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArinaWorld
+{
+    public class Item
+    {
+        public long PrototypeID { get; set; }
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public long Durable { get; set; }
+        public long Quantity { get; set; }
+    }
+}
---
+++ NinjaSato/ItemPrototype.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArinaWorld
+{
+    //[JsonConverter(typeof(StringEnumConverter))]
+    public enum CategoryList
+    {
+        None,
+        Food,
+        Livingware,
+        Tool,
+        Equipment,  
+        Material
+    }
+
+    //[JsonConverter(typeof(StringEnumConverter))]
+    public enum SubtypeList
+    {
+        Fruit,
+        Resource,
+        Weapon,
+        Shield,
+        Armor
+    }
+
+    public class ItemPrototype
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+
+        public CategoryList Category { get; set; }
+        public List<SubtypeList> Subtype { get; set; }
+        public long DurableMax { get; set; } = 100;
+        public long Value { get; set; }
+
+    }
+}
